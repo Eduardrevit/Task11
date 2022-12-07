f2fbe43ec0179f349eabfb97c6934066e0243765
user <user@WIN-8I41D9CN0BC>
2022-12-07 22:45:34 +0300

    Добавьте файлы проекта.

---
+++ Task11/Linur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    struct Linur
+    {
+        double k;
+        double b;
+
+        public Linur(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+            
+        }
+        public string Root()
+        {
+            if (k == 0)
+                return "Решений нет";
+            double x = -(b / k);
+          return $"Ответ Х = {x}";
+
+        }
+    }
+}
---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+         
+            Linur linur = new Linur(k, b);
+            Console.WriteLine(linur.Root());
+            Console.ReadKey();
+        }
+    }
+}
